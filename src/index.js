9da32769f5012fe86ce17cb666683f6e6692eaa7
Vaeb <vaebmail@gmail.com>
2017-07-26 00:12:06 +0100

    Running as daemon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ console.log('\n-STARTING-\n');
 
 const Auth = require('./auth.js');
 const NodeUtil = require('util');
-const Forever = require('forever-monitor');
+const Forever = require('forever');
 const DateFormat = require('dateformat');
 const Discord = require('discord.js');
 
@@ -14,6 +14,11 @@ const client = new Discord.Client({
 });
 
 const mainBotId = '224529399003742210';
+
+const admins = [
+    '107593015014486016', // Vaeb
+    '75743432164773888',  // Pkamara
+];
 
 // //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -41,44 +46,70 @@ function log(...args) {
 }
 
 let hasRestarted = false;
+let guildExists = true;
+
+function doStop() {
+    console.log(Forever.list());
+}
+
+function doRestart(guild, channel) {
+    if (!hasRestarted) {
+        hasRestarted = true;
+
+        log('[Auto] Restarted VaeBot');
+
+        const embedObj = new Discord.MessageEmbed()
+        .setTitle(channel ? 'Manual Protection' : 'Automatic Protection')
+        .setDescription(channel ? 'Restarting VaeBot in production mode' : 'VaeBot detected offline: Remotely restarting in production mode')
+        .setColor(0x00E676);
+
+        (channel || guild.defaultChannel).send(undefined, { embed: embedObj })
+        .catch(log);
+
+        /* const child = new (Forever.Monitor)('/home/flipflop8421/files/discordExp/VaeBot/index.js', {
+            max: 10,
+            silent: true,
+            args: [],
+        });
+
+        child.on('exit', () => {
+            log('VaeBot has exited after 10 restarts');
+        });
+
+        child.start(); */
+
+        console.log(Forever.startDaemon('/home/flipflop8421/files/discordExp/VaeBot/index.js'));
+    } else {
+        log('VaeBot not found online | Already restarted');
+        if (channel) {
+            const embedObj = new Discord.MessageEmbed()
+            .setTitle('Command Failed')
+            .setDescription('VaeBot has already been put into production mode')
+            .setColor(0x00E676);
+
+            guild.defaultChannel.send(undefined, { embed: embedObj })
+            .catch(log);
+        }
+    }
+}
 
 async function keepAlive() {
     const guild = client.guilds.first();
-    if (!guild) return log('[ERROR] Guild not found');
+    if (!guild) {
+        if (guildExists) {
+            guildExists = false;
+            log('[ERROR] Guild not found');
+        }
+        return false;
+    }
 
     const mainBot = await guild.fetchMember(mainBotId, false);
     const isOnline = mainBot ? mainBot.user.presence.status == 'online' : false;
 
     if (!isOnline) {
-        if (!hasRestarted) {
-            hasRestarted = true;
-
-            log('[Auto] Restarted VaeBot');
-
-            const embedObj = new Discord.MessageEmbed()
-            .setTitle('[StayAlive]')
-            .setDescription('VaeBot has been detected as offline and will now be automatically restarted')
-            .setColor(0x00E676);
-
-            guild.defaultChannel.send(undefined, { embed: embedObj })
-            .catch(log);
-
-            const child = new (Forever.Monitor)('/home/flipflop8421/files/discordExp/VaeBot/index.js', {
-                max: 10,
-                silent: true,
-                args: [],
-            });
-
-            child.on('exit', () => {
-                log('VaeBot has exited after 10 restarts');
-            });
-
-            child.start();
-        } else {
-            log('VaeBot not found online | Already restarted');
-        }
+        doRestart(guild);
     } else {
-        log('VaeBot found online');
+        // log('VaeBot found online');
         // if (hasRestarted) hasRestarted = false;
     }
 
@@ -99,6 +130,25 @@ client.on('disconnect', (closeEvent) => {
     log(`Code: ${closeEvent.code}`);
     log(`Reason: ${closeEvent.reason}`);
     log(`Clean: ${closeEvent.wasClean}`);
+});
+
+client.on('message', (msgObj) => {
+    const guild = msgObj.guild;
+    const speaker = msgObj.member;
+
+    if (guild == null || speaker == null || !admins.includes(speaker.id)) return;
+
+    const channel = msgObj.channel;
+    const contentLower = msgObj.content.toLowerCase();
+
+    switch (contentLower) {
+        case '!restart':
+            doRestart(guild, channel);
+            break;
+        case '!stop':
+            doStop();
+            break;
+    }
 });
 
 log('-CONNECTING-');
